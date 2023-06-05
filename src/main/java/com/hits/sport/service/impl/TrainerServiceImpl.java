236a92c1f443e630c67d114a009f074de94dffd2
Vinnichenko-Ivan <fox4080@gmail.com>
2023-06-05 10:52:01 +0700

    NewApi

--- src/main/java/com/hits/sport/service/impl/TrainerServiceImpl.java
+++ src/main/java/com/hits/sport/service/impl/TrainerServiceImpl.java
@@ -4,10 +4,12 @@ import com.hits.sport.dto.AddToTrainerUserDto;
 import com.hits.sport.dto.ShortTrainerDto;
 import com.hits.sport.exception.NotImplementedException;
 import com.hits.sport.service.TrainerService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class TrainerServiceImpl implements TrainerService {
     @Override
     public List<ShortTrainerDto> getTrainer(String shortName) {
